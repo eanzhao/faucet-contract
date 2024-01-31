e9cc910e02c1e6c7a8f86f2c0249244c4d156f37
eanzhao <yiqi.zhao@aelf.io>
2024-01-31 10:57:24 +0800

    Tune constants

--- src/Faucet.Constants.cs
+++ src/Faucet.Constants.cs
@@ -2,6 +2,6 @@ namespace AElf.Contracts.Faucet;
 
 public partial class Faucet
 {
-    private const long DefaultLimitAmount = 100_00000000;
-    private const long DefaultIntervalMinutes = 180; // 3 hours.
+    private const long DefaultLimitAmount = 10_00000000;
+    private const long DefaultIntervalMinutes = long.MaxValue; // On account can only take tokens once
 }
